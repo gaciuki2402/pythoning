ac3824f1612993a50cdff6315123c32afa831a25
gaciuki2402 <delbrivigrace@gmail.com>
2022-03-18 16:15:18 +0300

    while loops

---
+++ loops.py
@@ -0,0 +1,21 @@
+n=5
+while n>1:
+    n-=1
+    print(n)
+
+m=10
+while m>0:
+    m-=1
+    if m==6:
+        break
+    print(m)
+print("Loop ended")
+names=["Grace","Lizzy","Fortress","Valencia","Elsy","Janet"]
+while names:
+    if names=="Elsy":
+        break
+    names+=1
+    print(names)
+print("LOOP ENDED.")
+
+
