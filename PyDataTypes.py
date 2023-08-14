0807fc4eaa74563bf55986abbe5ae7f88174c9bd
qinyanjuidavid <davidkinyanjui052@gmail.com>
2023-08-14 21:58:42 +0300

    learning python datat types

---
+++ PyDataTypes.py
@@ -0,0 +1,43 @@
+#string
+#they're immutable
+t="Hello World"
+print(t)
+print(t[0])
+print(t[0:9])
+#srtings
+#they're mutable
+basket={"orange", "apple", "lemon", "pear","mango", "banana", "mango"}
+print(len(basket))
+print(basket)
+a =set("taitataveta")
+print(a)
+#numbers
+int_num=10
+print(int_num)
+float_num=20.4
+print(float_num)
+complex_num=3.42j
+print(complex_num)
+#list
+#They're mutable
+list=[123,'ello',10.3,'f',"emmy"]
+print(list)
+print(list[2])
+print(list[0:3])
+print(list *2)
+#dictionary
+#they're mutable
+dic={"name":"wally", "age":18}
+print(dic)
+print(dic['name'])
+print(dic["age"])
+print(dic.values())
+print(dic.keys())
+#tuples
+#they're immutable
+tuple=(123, "mochi", 'wally', 35.96)
+print(tuple)
+print(tuple[-1])
+
+
+
